c081955dba6a483e1a03a3c7970c8029abef3724
winjeysong <winjey-song@163.com>
2017-04-26 23:57:43 +0800

    Add tmsf file#20170426-2357

--- tmsf/tmsf/items.py
+++ tmsf/tmsf/items.py
@@ -11,8 +11,10 @@ import scrapy
 class TmsfItem(scrapy.Item):
     name = scrapy.Field()
     ad_name = scrapy.Field()
+    type = scrapy.Field()
     location = scrapy.Field()
     news = scrapy.Field()
     price = scrapy.Field()
     tel = scrapy.Field()
     link = scrapy.Field()
+    avail = scrapy.Field()
--- tmsf/tmsf/spiders/tmsf_spider.py
+++ tmsf/tmsf/spiders/tmsf_spider.py
@@ -1,9 +1,35 @@
 # _*_ coding:utf-8 _*_
 
+import string
 import scrapy
-from tutorial.items import TmsfItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from tmsf.items import TmsfItem
 
 
-class TmsfSpider(scrapy.spider.Spider):
+class TmsfSpider(CrawlSpider):
     name = "tmsf"
-    allow_domains = "http://www.tmsf.com"
+    allow_domains = ["http://www.tmsf.com"]
+    start_urls = [
+        "http://www.tmsf.com/newhouse/property_searchall.htm?"
+    ]
+
+    rules = (
+        Rule(LinkExtractor(allow=r'\?page=.*'),callback='parse_next')
+    )
+
+    def parse_next(self, response):
+        for sel in response.xpath('//div[@class="build_des dingwei"]'):
+            item = TmsfItem()
+            item['name'] = sel.xpath('//div[@class="build_word01"]/text()').extract()
+            item['ad_name'] = sel.xpath('//div[@class="build_word01"]/div[@class="fl line26 mgl black"]/text()').extract()
+            item['type'] = sel.xpath('//div[@class="build_txt01"]/p[@class="build_txt03 colormg"]/text()').extract()
+            item['location'] = sel.xpath('//div[@class="build_txt01"]/p[@class="build_txt03 outof colormg"]/text()').extract()
+            item['news'] = sel.xpath('//div[@class="build_txt01"]/p[@class="build_txt08 outof colorlg"]/text()').extract()
+            # item['price'] = sel.xpath('div[@class="site-descr "]/text()').extract()
+            item['tel'] = sel.xpath('//font[@class="colordg"]/font[@class="colordg"]/text()').extract()
+            item['link'] = sel.xpath('//div[@class="dingwei"]/a/@href').extract()
+            item['avail'] = sel.xpath('//div[@class="howsell"]//a/text()').extract()
+            yield item
+
+    # def to_num(self, span_class):
