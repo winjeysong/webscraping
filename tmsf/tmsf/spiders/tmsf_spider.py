597791d9b4b50bbd1851ec06b30fe0b54a1c6af4
winjeysong <winjey-song@163.com>
2017-05-07 23:02:00 +0800

    Some changes
    1. Add a new def: change the css number to real number
    2. Split joint the url
    3. item[price] matched successfully

--- tmsf/tmsf/spiders/tmsf_spider.py
+++ tmsf/tmsf/spiders/tmsf_spider.py
@@ -22,20 +22,23 @@ class TmsfSpider(scrapy.spiders.Spider):
             yield scrapy.Request(url, headers=headers, callback=self.parse_next)
     '''
 
+    # 将CSS数字替换成数字
+    def rep_num(self, num):
+        new_num = num.replace('numbbone','1').replace('numbbtwo','2').replace('numbbthree','3').replace('numbbfour','4').replace('numbbfive','5').replace('numbbsix','6').replace('numbbseven','7').replace('numbbeight','8').replace('numbbnine','9').replace('numbbzero','0')
+        return new_num
+
     def parse(self, response):
         items = []
         for sel in response.xpath('//div[@class="build_des dingwei"]'):
             item = TmsfItem()
-            item['name'] = sel.xpath('div[@class="build_txt line26"]/div[@class="build_word01"]/text()').extract()
-            item['ad_name'] = sel.xpath('div[@class="build_txt line26"]/div[@class="build_word01"]/div[@class="fl line26 mgl black"]/text()').extract()
+            item['name'] = sel.xpath('div[@class="build_pic"]/div[@class="dingwei"]/a/img/@title').extract()
             item['type'] = sel.xpath('div[@class="build_txt line26"]/div[@class="build_txt01"]/p[@class="build_txt03 colormg"]/text()').extract()
             item['location'] = sel.xpath('div[@class="build_txt line26"]/div[@class="build_txt01"]/p[@class="build_txt03 outof colormg"]/text()').extract()
-            # item['price'] = sel.xpath('').extract()
+            item['price'] = map(self.rep_num, [''.join(sel.xpath('div[@class="word1"]/span').re(r'<span class="(.*)">'))])
             item['tel'] = sel.xpath('div[@class="build_txt05 txt05h25"]/font[@class="colordg"]/font[@class="colordg"]/text()').extract()
-            item['link'] = [u"http://www.tmsf.com"] + sel.xpath('div[@class="build_pic"]/div[@class="dingwei"]/a/@href').extract()
+            item['link'] = [u"http://www.tmsf.com" + ''.join(sel.xpath('div[@class="build_pic"]/div[@class="dingwei"]/a/@href').extract())]
             item['avail'] = sel.xpath('div[@class="build_pic"]/div[@class="howsell"]//font[@class="colormg"]/a/text()').extract()
             # yield item
             items.append(item)
         return items
-
 
