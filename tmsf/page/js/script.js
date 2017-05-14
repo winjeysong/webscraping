414313ba7b1aaec8bf6c252633751310ac72915d
winjeysong <winjey-song@163.com>
2017-05-15 00:17:58 +0800

    Not finished#20170515-0017

--- tmsf/page/js/script.js
+++ tmsf/page/js/script.js
@@ -4,15 +4,35 @@
  * 2017.5
  */
 
-//generate AMap
+/* create a map instance */
 var map = new AMap.Map('container',{
     resizeEnable: true,
-    zoom: 10,
-    center: [116.480983, 40.0958]
+    zoom: 11
 });
+map.setMapStyle("fresh")  //set the map style
+map.setFeatures(["road", "point", "bg"])  //set the map content
 
-//set the map style
-map.setMapStyle("fresh")
+/* mark datas on map */
+//function to get location 
+function geocoder(){  
+    //load AMap plugin
+    AMap.service(["AMap.Geocoder"],function(){
+        //create an instance
+        var place = new AMap.Geocoder({
+            city: "hangzhou",
+            redius: 1000,
+        });
 
-//set the map content
-map.setFeatures(["road", "point", "bg"])
+        //call the method
+        place.getLocation("",function(status, result){
+            if(status == "complete" && result.info ==="OK"){
+                geocoder_CallBack(result);
+            }
+        });
+    });
+}
+
+//function to generate marker
+function generateMarker(index, data){  
+    var marker = new
+}
