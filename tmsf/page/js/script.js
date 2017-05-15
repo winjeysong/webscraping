fbc48a37c07468f903978df7fe57c36a8e470008
winjeysong <winjey-song@163.com>
2017-05-16 01:20:05 +0800

    Wait for importing json datas#20170516-0120

--- tmsf/page/js/script.js
+++ tmsf/page/js/script.js
@@ -4,6 +4,11 @@
  * 2017.5
  */
 
+/* datas */
+var datas = [
+
+];
+
 /* create a map instance */
 var map = new AMap.Map('container',{
     resizeEnable: true,
@@ -23,16 +28,49 @@ function geocoder(){
             redius: 1000,
         });
 
-        //call the method
-        place.getLocation("",function(status, result){
-            if(status == "complete" && result.info ==="OK"){
-                geocoder_CallBack(result);
-            }
-        });
+        //get data in Array datas, call the method to get every location result
+        for(i in datas){
+            place.getLocation(datas[i].name,function(status, result){
+                if(status == "complete" && result.info ==="OK"){
+                    geocoder_CallBack(result);
+                }
+            });
+        }
+       
     });
 }
 
 //function to generate marker
-function generateMarker(index, data){  
-    var marker = new
+function generateMarker(i, d){  
+    //create an instance of marker
+    var marker = new AMap.Marker({
+        map: map,
+        position: [d.location.getLng(), d.location.getLat()]
+    });
+    //create an instance of data display
+    var dataDisplay = new AMap.InfoWindow({
+        //content: d.formattedAddress, 静态内容
+        offset: {x:0 ,y:-30}
+    });
+    //set content 
+    dataDisplay.setContent(
+        //String或htmlDOM,htmlDOM也需要写在引号内
+        ""
+    )
+    //add event
+    marker.on("mouseover",function(e){
+        dataDisplay.open(map, marker.getPosition());
+    })
+}
+
+//callback from getLocation, and call generateMarker
+function geocoder_CallBack(result){
+    //get geocode
+    var geocode = result.geocodes;
+    for(var i =0; i<geocode.length; i++){
+        //call generateMarker
+        generateMarker(i,geocode[i]);
+    }
+    //make marker in a fit range of page
+    map.setFitView();
 }
